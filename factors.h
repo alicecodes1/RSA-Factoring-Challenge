6e9328e6232c7e1ad4bacbeefb0cd51f15f3eb48
Seriki Ifeoluwa <ifeoluwaalice75@gmail.com>
2023-06-03 15:47:15 -0700

    RSA factoring challenge

---
+++ factors.h
@@ -0,0 +1,10 @@
+#ifndef FACTORS_H
+#define FACTORS_H
+
+#include <vector>
+#include <string>
+
+std::vector<std::pair<int, int>> factorize(int number);
+void factorize_file(const std::string &file_path);
+
+#endif /* FACTORS_H */
